178d1f5e124f5eca0f33ddb74321b6203daed170
jotate70 <jotate70@gmail.com>
2021-11-11 13:03:12 -0500

    carga de modulo helpdesk ticket custom

---
+++ helpdesk_ticket_custom/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "helpdesk ticket custom",
+
+    'summary': """
+        This module creates new models and fields to extend the functionality of the helpdesk tickets
+        """,
+
+    'description': """
+        Module that extends functionality in the helpdesk module
+    """,
+
+    'author': "Andirent - Julián Toscano",
+    'website': "https://www.andirent.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'helpdesk',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['helpdesk'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
