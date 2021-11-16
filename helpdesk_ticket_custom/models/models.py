334cc325a10c0e48c48ffe187e55cfe2c663db6d
jotate70 <42453302+jotate70@users.noreply.github.com>
2021-11-16 14:29:07 -0500

    Add files via upload
    Actualización

--- helpdesk_ticket_custom/models/models.py
+++ helpdesk_ticket_custom/models/models.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, exceptions
 
+# heredamos del modelo helpdesk team
+class helpdesk_team_inherit(models.Model):
+    _inherit = 'helpdesk.team'
+
+    x_visibility = fields.Boolean(string='Visibilidad de clasificación')
+
 # heredamos del modelo de tickets de mesa ayuda
 class helpdesk_ticket_extended(models.Model):
     _inherit = 'helpdesk.ticket'
 
+    x_visibility_related = fields.Boolean(string='Campo oculto', related='team_id.x_visibility', store=True, readonly=True)
+    x_classification = fields.Many2one(comodel_name='helpdesk_classification', string='clasificación')
     x_project = fields.Many2one(comodel_name='helpdesk_project', string='Proyecto', required="True")
     x_family = fields.Many2one(comodel_name='helpdesk_family', string='Familia', required="True")
     x_sub_group = fields.Many2one(comodel_name='helpdesk_sub_group', string='Sub grupo', required="True")
@@ -20,10 +28,23 @@ class helpdesk_users(models.Model):
 
     x_project = fields.Many2many(comodel_name='helpdesk_project', relation='helpdesk_project_relation', columnn1='id', columnn2='name', string='Proyecto')
 
-# creamos modelo proyecto
+# Se crea modelo clasificación
+class helpdesk_classification(models.Model):
+    _name = 'helpdesk_classification'
+    _description = 'Clasificación en mesa de ayuda'
+
+    name = fields.Char(string='Nombre', required="True")
+    x_code = fields.Char(string='Código', required="True")
+
+    # Restricción a nivel de SQL
+    _sql_constraints = [
+        ('name_unique', 'UNIQUE(name)', 'La clasificación ya existe'),
+    ]
+
+# Se crea modelo proyecto
 class helpdesk_project(models.Model):
     _name = 'helpdesk_project'
-    _description = 'Familia en mesa de ayuda'
+    _description = 'Proyecto en mesa de ayuda'
 
     name = fields.Char(string='Nombre', required="True")
     x_code = fields.Char(string='Código', required="True")
@@ -33,11 +54,7 @@ class helpdesk_project(models.Model):
         ('name_unique', 'UNIQUE(name)', 'El proyecto ya existe'),
     ]
 
-    _sql_constraints = [
-        ('x_code_unique', 'UNIQUE(x_code)', 'El codigo debe ser unico'),
-    ]
-
-# creamos modelo familia
+# Se crea modelo family
 class helpdesk_family(models.Model):
     _name = 'helpdesk_family'
     _description = 'Familia en mesa de ayuda'
@@ -51,11 +68,7 @@ class helpdesk_family(models.Model):
         ('name_unique', 'UNIQUE(name)', 'La familia ya existe'),
     ]
 
-    _sql_constraints = [
-        ('x_code_unique', 'UNIQUE(x_code)', 'El codigo debe ser unico'),
-    ]
-
-# creamos modelo subgrupo
+# Se crea modelo sub grupo
 class helpdesk_sub_group(models.Model):
     _name = 'helpdesk_sub_group'
     _description = 'Sub grupo en mesa de ayuda'
