50ae6fadb5f09d69c3ca0651f7c3e7f77c716352
jotate70 <42453302+jotate70@users.noreply.github.com>
2022-02-16 10:40:21 -0500

    Update 15.1.1 andirent_theme
    background correction in mobile mode and burger menu color

--- andirent_theme/__manifest__.py
+++ andirent_theme/__manifest__.py
@@ -11,10 +11,14 @@
     'name': "andirent_theme",
 
     'summary': """
-        15.0.2 the color navy blue, green and andirenmt background is added""",
+        15.0.2 the color navy blue, green and andirenmt background is added.
+        15.1.0 The odoo icon is replaced by the andirent icon in desktop and mobile versions.
+        15.1.1 background correction in mobile mode and burger menu color
+    
+        """,
 
     'description': """
-        theme that adds andirent colors
+        theme that adds andirent colors.
     """,
 
     'author': "Andirent  Author: Julián Toscano",
@@ -30,6 +34,10 @@
         'web_enterprise',
     ],
 
+    'data': [
+        'views/webclient_templates_custom.xml',
+    ],
+
     'installable': True,
     'application': True,
 
@@ -37,7 +45,12 @@
         'web._assets_common_styles': [
             ('replace', 'web_enterprise/static/src/legacy/scss/primary_variables.scss', 'andirent_theme/static/src/legacy/scss/primary_variables.scss'),
             ('replace', 'web_enterprise/static/src/legacy/scss/ui.scss', 'andirent_theme/static/src/legacy/scss/ui.scss'),
+            # ('replace', 'web_enterprise/static/src/webclient/burger_menu/burger_menu.scss', 'andirent_theme/static/src/webclient/burger_menu/burger_menu.scss'),
         ],
+
+        'web.assets_backend': [
+            ('replace', 'web_enterprise/static/src/webclient/burger_menu/burger_menu.scss', 'andirent_theme/static/src/webclient/burger_menu/burger_menu.scss'),
+        ]
     },
 
     'license': 'LGPL-3',
