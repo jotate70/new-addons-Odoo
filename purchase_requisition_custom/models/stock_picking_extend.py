92153943850d27615d6eb7ca2930e21d5ef4707d
Julián Toscano <42453302+jotate70@users.noreply.github.com>
2022-05-02 02:13:02 -0500

    Update 15.3.0 purchase_requisition_custom
    This versions contains stock movement in two steps, origin location to transit location, transit location to destination location.

--- purchase_requisition_custom/models/stock_picking_extend.py
+++ purchase_requisition_custom/models/stock_picking_extend.py
@@ -1,5 +1,5 @@
 from odoo import fields, models, api
-
+from odoo.exceptions import UserError
 
 class stock_picking_extend(models.Model):
     _inherit = 'stock.picking'
@@ -8,22 +8,83 @@ class stock_picking_extend(models.Model):
     code = fields.Selection([('incoming', 'Receipt'), ('outgoing', 'Delivery'), ('internal', 'Internal Transfer')],
                             'Operación', related='picking_type_id.code')
     warehouse_id = fields.Many2one(comodel_name='stock.warehouse', string='A almacén', related='location_dest_id.warehouse_id')
-    activity_id = fields.Integer(string='id actividad', store=True)     # id de la actividad asignada
+    activity_id = fields.Integer(string='id actividad')     # id de la actividad asignada
+    ticket_many2many = fields.Many2many(comodel_name='helpdesk.ticket',
+                                        relation='x_helpdesk_ticket_purchase_requisition_rel',
+                                        column1='purchase_requisition_id', column2='helpdesk_ticket_id',
+                                        string='Tickets', related='requisition_id.ticket_many2many')
+    account_analytic_id = fields.Many2one(comodel_name='account.analytic.account', string='Cuenta Analítica',
+                                          related='location_dest_id.account_analytic_id', store=True)
+    stage = fields.Integer(string='Etapa')
+    validation = fields.Integer(string='Validacación', help='Permite validar el stock picking de transito a destino')
+    parent_stock_picking = fields.Many2one(comodel_name='stock.picking', string='Stock picking padre')
+
+    # Se crea apunte analítico
+    def compute_account_analytic_cost(self):
+        # Condición para solo registrar apuntes analiticos donde no existe ordenes de compra y venta
+        if self.purchase_id:
+            return True
+        elif self.sale_id:
+            return True
+        else:
+            # Permite determinar si el importe es negativo o positivo (debita/acredita) mediante el tipo de operación
+            if self.code == 'internal':
+                a = 1
+            elif self.code == 'outgoing':
+                a = 1
+            elif self.code == 'incoming':
+                a = -1
+            # Crea apunte analítico
+            for rec in self.move_ids_without_package:
+                create_account_analytic = {
+                    'name': rec.description_picking,
+                    'account_id': rec.account_analytic_id.id,
+                    'partner_id': self.partner_id.id,
+                    'date': fields.datetime.now(),
+                    'company_id': self.env.company.id,
+                    'amount': rec.standard_price_t*a,
+                    'unit_amount': rec.product_uom_qty,
+                    'product_id': rec.product_id.id,
+                    'product_uom_id': rec.product_uom.id,
+                    'stock_picking_line_id': rec.id,
+                }
+                self.env['account.analytic.line'].sudo().create(create_account_analytic)
 
     # Actualización de la función del boton como por hacer
     def action_confirm(self):
-        self._check_company()
-        self.mapped('package_level_ids').filtered(lambda pl: pl.state == 'draft' and not pl.move_ids)._generate_moves()
-        # call `_action_confirm` on every draft move
-        self.mapped('move_lines')\
-            .filtered(lambda move: move.state == 'draft')\
-            ._action_confirm()
+        if self.parent_stock_picking:
+            if self.parent_stock_picking.stage == 1 and self.parent_stock_picking.state == 'done':
+                self._check_company()
+                self.mapped('package_level_ids').filtered(
+                    lambda pl: pl.state == 'draft' and not pl.move_ids)._generate_moves()
+                # call `_action_confirm` on every draft move
+                self.mapped('move_lines') \
+                    .filtered(lambda move: move.state == 'draft') \
+                    ._action_confirm()
 
-        # run scheduler for moves forecasted to not have enough in stock
-        self.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done'))._trigger_scheduler()
+                # run scheduler for moves forecasted to not have enough in stock
+                self.mapped('move_lines').filtered(
+                    lambda move: move.state not in ('draft', 'cancel', 'done'))._trigger_scheduler()
 
-        #  Marca actividad como hecha de forma automatica
-        new_activity = self.env['mail.activity'].search([('id', '=', self.activity_id)], limit=1)
-        new_activity.action_feedback(feedback='Es confirmada')
-        return True
+                #  Marca actividad como hecha de forma automatica
+                new_activity = self.env['mail.activity'].search([('id', '=', self.activity_id)], limit=1)
+                new_activity.action_feedback(feedback='Es confirmada')
+                return True
+            else:
+                raise UserError('Debe terminar primero la transferencia de ubicación origen a transito.')
+        else:
+            self._check_company()
+            self.mapped('package_level_ids').filtered(lambda pl: pl.state == 'draft' and not pl.move_ids)._generate_moves()
+            # call `_action_confirm` on every draft move
+            self.mapped('move_lines')\
+                .filtered(lambda move: move.state == 'draft')\
+                ._action_confirm()
+
+            # run scheduler for moves forecasted to not have enough in stock
+            self.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done'))._trigger_scheduler()
+
+            #  Marca actividad como hecha de forma automatica
+            new_activity = self.env['mail.activity'].search([('id', '=', self.activity_id)], limit=1)
+            new_activity.action_feedback(feedback='Es confirmada')
+            return True
 
--- purchase_requisition_custom/models/stock_warehouse_extend.py
+++ purchase_requisition_custom/models/stock_warehouse_extend.py
@@ -1,12 +1,52 @@
 from odoo import fields, models, api
 from odoo.exceptions import UserError
+import json
 
 class stock_warehouse_extend(models.Model):
     _inherit = 'stock.warehouse'
 
+    usage = fields.Selection([
+        ('supplier', 'Almacen Cliente'),
+        ('internal', 'Almacen Interno'),
+        ('customer', 'Almacen Proveedor')], string='Tipo de almacén',
+        default='internal', index=True, required=True,
+        help="* Vendor Location: Virtual location representing the source location for products coming from your vendors"
+             "\n* Internal Location: Physical locations inside your own warehouses,"
+             "\n* Customer Location: Virtual location representing the destination location for products sent to your customers")
+
     employee_id = fields.Many2many(comodel_name='hr.employee', relation='x_hr_employee_stock_warehouse_rel',
                      column1='stock_warehouse_id', column2='hr_employee_id', string='Empleado responsable',
                      help='Empleado que puede aprobar transferencias internas en el almacen')
+    location_id = fields.Many2one(comodel_name="location_warehouse", string='Locación', required=True,
+                                  help="Indica la locación/ciudad donde se encuentra el almacen")
+    available_requisition = fields.Boolean(string='Puede usarse en requisiciones')
+    code = fields.Char('Short Name', required=True, size=8, help="Short name used to identify your warehouse")
+    transit_location_id_domain = fields.Char(compute='_domain_transit_location_id', readonly=True, store=False)
+    transit_location_id = fields.Many2one(comodel_name='stock.location', string='Ubicación de transito',
+                                          help='Solo se permite una ubicación de transito por almacen')
+    transit_location = fields.Boolean(string='Ubicación de transito', compute='_compute_transit_location',
+                                      help='Solo se permite una ubicación de transito por almacen', readonly=True)
+
+    # Función reestablecer campo available_requisition
+    @api.onchange('usage')
+    def _reset_available_requisition(self):
+        if self.usage != 'supplier':
+            self.available_requisition = False
+
+    # Función que aplica filtro dinamico
+    @api.depends('transit_location_id')
+    def _domain_transit_location_id(self):
+        for rec in self:
+            rec.transit_location_id_domain = json.dumps(
+                [('warehouse_id', '=', self.ids), ('usage', '=', 'transit')])
+
+    # validación limite para asociar almacen
+    @api.depends('transit_location_id')
+    def _compute_transit_location(self):
+        if self.transit_location_id:
+            self.transit_location = True
+        else:
+            self.transit_location = False
 
     # validación limite para asociar almacen
     @api.onchange('employee_id')
