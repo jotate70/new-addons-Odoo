a787691e9374c7f74afa0b89cb67ca55f99b0092
Julián Toscano <42453302+jotate70@users.noreply.github.com>
2022-05-20 09:28:08 -0500

    Delete

--- purchase_requisition_custom/models/users_extend.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class users_extend(models.Model):
-    _inherit = 'res.users'
-
-    employee_id = fields.Many2one(comodel_name='hr.employee', string='Empleado relacionado')
-
